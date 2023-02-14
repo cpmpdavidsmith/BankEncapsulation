d18218235d019e84cd0c4ccb3ec8f3ff94013b02
David Smith <cpmpdavidsmith@icloud.com>
2023-02-14 16:21:03 -0500

    Bank Encapsulation2

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        //private field set to 0
+        private double _balance = 0;
+
+        public double Deposit(double amount)
+        {
+            return _balance += amount;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }	
+}
+
